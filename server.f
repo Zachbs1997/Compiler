bb08fc6da8796e2abe6c27cc188b88dca4fa2480
Zachbs1997 <48690930+Zachbs1997@users.noreply.github.com>
2019-05-10 17:00:24 -0500

    Add files via upload

--- server.f
+++ server.f
@@ -1,4 +1,9 @@
-var y = 50;
-var t = false;
-var id = "id";
-var unInitiated;
+int y = 50;
+bool t = false;
+string id = "id";
+string numberButString = "50 + 50";
+int math = 75 * 75;
+/* just ignore this please 5445 safdfasd * & ( / ) // */
+//int mathWithVariable = math / y + 5;
+string strConcat = "hello " + "world";
+string unInitiated;
